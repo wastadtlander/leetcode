d0da79f00aa8e424b43f139743a139d60b8bb954
William Stadtlander <66275962+wastadtlander@users.noreply.github.com>
2022-12-02 09:46:57 -0800

    Create m_73.cpp

---
+++ cpp/m_73.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        bool columnZero = false;
+
+        for (int i = 0; i < matrix.size(); i++) {
+            if (matrix[i][0] == 0) {
+                columnZero = true;
+            }
+            for (int j = 1; j < matrix[0].size(); j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0, matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = matrix.size() - 1; i >= 0; i--) {
+            for (int j = matrix[0].size() - 1; j >= 1; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+            if (columnZero) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
