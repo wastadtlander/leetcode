b35712efa45e8fc079527089729b8629f8b16ad3
William Stadtlander <66275962+wastadtlander@users.noreply.github.com>
2022-12-04 23:07:59 -0500

    Create e_876.cpp

---
+++ cpp/e_876.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *walk = head, *run = head;
+        while (run && run->next) {
+            walk = walk->next, run = run->next->next;
+        }
+        return walk;
+    }
+};
