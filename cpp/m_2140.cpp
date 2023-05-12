4d27f1023b2ee3fb5a98fa12fa4ec169713a2caa
William Stadtlander <66275962+wastadtlander@users.noreply.github.com>
2023-05-11 22:05:51 -0400

    Create m_2140.cpp

---
+++ cpp/m_2140.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long mostPoints(vector<vector<int>>& questions) {
+        vector<long long> dp(200001);
+
+        for (int i = questions.size() - 1; i >= 0; i--) {
+            dp[i] = max(questions[i][0] + dp[questions[i][1] + i + 1], dp[i + 1]);
+        }
+
+        return dp[0];
+    }
+};
