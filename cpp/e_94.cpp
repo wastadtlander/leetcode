49cb0257441d7e0cc3dd0f31c5d44a581e4badcf
William Stadtlander <66275962+wastadtlander@users.noreply.github.com>
2023-11-20 23:57:15 -0500

    Create e_94.cpp

---
+++ cpp/e_94.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        if (!root) return {};
+        vector<int> ret;
+        inorder(root->left, ret);
+        ret.push_back(root->val);
+        inorder(root->right, ret);
+        return ret;
+    }
+
+    void inorder(TreeNode* root, vector<int>& ret) {
+        if (!root) return;
+        inorder(root->left, ret);
+        inorder(root->right, ret);
+        ret.push_back(root->val);
+    }
+};
